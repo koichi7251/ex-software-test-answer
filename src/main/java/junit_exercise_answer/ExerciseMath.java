919a42ae1778bce50a7923d1e75c20b38cd3f123
Masayuki IGA <igamasayuki@gmail.com>
2018-11-12 15:47:21 +0900

    変数名を問題文に合わせた

--- src/main/java/junit_exercise_answer/ExerciseMath.java
+++ src/main/java/junit_exercise_answer/ExerciseMath.java
@@ -1,17 +1,17 @@
 package junit_exercise_answer;
 
 public class ExerciseMath {
-	public static int factorial(int args) {
-		if (args <= 0) {
+	public static int factorial(int n) {
+		if (n <= 0) {
 			throw new IllegalArgumentException("0以下の値は不正です。");
 		}
 
-		if (13 <= args) {
+		if (13 <= n) {
 			throw new IllegalArgumentException("13以上の値は不正です。");
 		}
 
 		int answer = 1;
-		for (int i = 1; i <= args; i++) {
+		for (int i = 1; i <= n; i++) {
 			answer *= i;
 		}
 		return answer;
